546656ca7fec8c271c79cb7c6211bf33e092af31
Lespingal <pierre.lespingal@gmail.com>
2023-02-27 20:33:21 +0100

    feat: Improve controls layout sizing

--- src/lib/Panel/Control.svelte
+++ src/lib/Panel/Control.svelte
@@ -8,14 +8,14 @@
 	export let update: (newMapSize: number) => void;
 </script>
 
-<div class="w-full h-16 sm:h-32 flex items-center justify-center gap-x-16">
-	<div class="w-16 sm:w-32 p-2 cursor-pointer" on:click={() => update(mapSize)} on:keydown={() => update(mapSize)}>
-		<img class="w-14 sm:w-28" src={reload} alt="settings" />
+<div class="w-full h-16 sm:h-24 flex items-center justify-center gap-x-16">
+	<div class="w-16 sm:w-24 p-2 cursor-pointer" on:click={() => update(mapSize)} on:keydown={() => update(mapSize)}>
+		<img class="w-14 sm:w-20" src={reload} alt="settings" />
 	</div>
 
 	<div class="flex justify-evenly">
 		<div
-			class="w-10 sm:w-20 flex items-center p-2 cursor-pointer"
+			class="w-10 sm:w-16 flex items-center p-2 cursor-pointer"
 			on:click={() => {
 				mapSize = Math.max(3, mapSize - 1);
 			}}
@@ -23,13 +23,13 @@
 				mapSize = Math.max(3, mapSize - 1);
 			}}
 		>
-			<img class="w-8 sm:w-16" src={minus} alt="minus" />
+			<img class="w-8 sm:w-14" src={minus} alt="minus" />
 		</div>
-		<div class="w-10 sm:w-20 flex items-center justify-center p-2 align-bottom text-xl sm:text-3xl">
+		<div class="w-10 sm:w-16 flex items-center justify-center p-2 align-bottom text-xl sm:text-3xl">
 			<p>{mapSize}</p>
 		</div>
 		<div
-			class="w-10 sm:w-20 flex items-center p-2 cursor-pointer"
+			class="w-10 sm:w-16 flex items-center p-2 cursor-pointer"
 			on:click={() => {
 				mapSize = Math.min(42, mapSize + 1);
 			}}
@@ -37,7 +37,7 @@
 				mapSize = Math.min(42, mapSize + 1);
 			}}
 		>
-			<img class="w-8 sm:w-16" src={plus} alt="plus" />
+			<img class="w-8 sm:w-14" src={plus} alt="plus" />
 		</div>
 	</div>
 </div>
